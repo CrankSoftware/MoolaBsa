dd14306bfb055fe8a8d4051bc3343ea2239b1640
Leshi Chen <chenleshi@hotmail.com>
2016-07-10 00:10:47 +1200

    added two new models for next week

---
+++ Moola.Bsa.Logic/Models/FinanceWithdrawalsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moola.Bsa.Logic.Interfaces.Input;
+using Moola.Bsa.Logic.Interfaces.Output;
+using Moola.Bsa.Logic.Models;
+
+namespace Moola.Bsa.Logic.Models
+{
+    public class FinanceWithdrawalsModel : BaseModel
+    {
+        public override IModelOutput Analyze(IModelInput input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Moola.Bsa.Logic/Models/GamblingModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moola.Bsa.Logic.Interfaces.Input;
+using Moola.Bsa.Logic.Interfaces.Output;
+
+namespace Moola.Bsa.Logic.Models
+{
+    public class GamblingModel : BaseModel
+    {
+        public override IModelOutput Analyze(IModelInput input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
